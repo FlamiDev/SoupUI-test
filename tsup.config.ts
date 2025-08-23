aaef7d0d50993f47e808425ba6eaa38094828e74
FlamiDev <git@flami.dev>
2025-08-23 14:03:59 +0200

    Include css in output

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,42 +1,36 @@
-import { defineConfig } from 'tsup'
+import {defineConfig, Options} from 'tsup'
 import * as preset from 'tsup-preset-solid'
 
 const preset_options: preset.PresetOptions = {
-  // array or single object
-  entries: [
-    // default entry (index)
-    {
-      // entries with '.tsx' extension will have `solid` export condition generated
-      entry: 'src/index.tsx',
-      // will generate a separate development entry
-      dev_entry: true,
+    entries: {
+        entry: 'src/index.tsx',
     },
-  ],
-  // Set to `true` to remove all `console.*` calls and `debugger` statements in prod builds
-  drop_console: true,
-  // Set to `true` to generate a CommonJS build alongside ESM
-  // cjs: true,
+    drop_console: true,
 }
 
 const CI =
-  process.env['CI'] === 'true' ||
-  process.env['GITHUB_ACTIONS'] === 'true' ||
-  process.env['CI'] === '"1"' ||
-  process.env['GITHUB_ACTIONS'] === '"1"'
+    process.env['CI'] === 'true' ||
+    process.env['GITHUB_ACTIONS'] === 'true' ||
+    process.env['CI'] === '"1"' ||
+    process.env['GITHUB_ACTIONS'] === '"1"'
 
-export default defineConfig(config => {
-  const watching = !!config.watch
+export default defineConfig(async config => {
+    const watching = !!config.watch
 
-  const parsed_options = preset.parsePresetOptions(preset_options, watching)
+    const parsed_options = preset.parsePresetOptions(preset_options, watching)
 
-  if (!watching && !CI) {
-    const package_fields = preset.generatePackageExports(parsed_options)
+    if (!watching && !CI) {
+        const package_fields = preset.generatePackageExports(parsed_options)
+        await preset.writePackageJson(package_fields)
+    }
 
-    console.log(`package.json: \n\n${JSON.stringify(package_fields, null, 2)}\n\n`)
+    const options = preset.generateTsupOptions(parsed_options)
+    console.log(options)
 
-    // will update ./package.json with the correct export fields
-    preset.writePackageJson(package_fields)
-  }
-
-  return preset.generateTsupOptions(parsed_options)
+    return options.map(opt => ({
+        ...opt,
+        clean: true,
+        minify: !watching,
+        injectStyle: true,
+    } as Options))
 })
